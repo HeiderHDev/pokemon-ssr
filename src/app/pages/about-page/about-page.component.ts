c0532cbdd04af006a4a08ba1be7904b75edfe3cf
HeiderHDev <heiderhrnndz@gmail.com>
2024-11-03 21:55:34 -0500

    feat: 🎸 About page component

--- src/app/pages/about-page/about-page.component.ts
+++ src/app/pages/about-page/about-page.component.ts
@@ -1,4 +1,10 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+} from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   standalone: true,
@@ -6,4 +12,28 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './about-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class AboutPageComponent {}
+export default class AboutPageComponent implements OnInit {
+  private readonly title = inject(Title);
+  private readonly meta = inject(Meta);
+
+  ngOnInit(): void {
+    this.title.setTitle('About');
+    this.meta.updateTag({
+      name: 'description',
+      content: 'About page description',
+    });
+    this.meta.addTag({
+      name: 'description',
+      content: 'About page description',
+    });
+    this.meta.addTag({
+      name: 'og:title',
+      content: 'About Page',
+    });
+    this.meta.addTag({
+      name: 'keywords',
+      content:
+        'about, page, description, Heider, Angular, DevsCO, Angular Pro, HeiderDev',
+    });
+  }
+}
