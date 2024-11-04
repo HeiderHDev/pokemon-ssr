b4c3b6ec6f4371682056211c9a5e529c5dae4227
HeiderHDev <heiderhrnndz@gmail.com>
2024-11-03 21:55:17 -0500

    feat: 🎸 Contact page component

--- src/app/pages/contact-page/contact-page.component.ts
+++ src/app/pages/contact-page/contact-page.component.ts
@@ -1,4 +1,10 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+} from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-contact-page',
@@ -7,4 +13,28 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './contact-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class ContactPageComponent {}
+export default class ContactPageComponent implements OnInit {
+  private readonly title = inject(Title);
+  private readonly meta = inject(Meta);
+
+  ngOnInit(): void {
+    this.title.setTitle('Contact');
+    this.meta.updateTag({
+      name: 'description',
+      content: 'Contact page description',
+    });
+    this.meta.addTag({
+      name: 'description',
+      content: 'Contact page description',
+    });
+    this.meta.addTag({
+      name: 'og:title',
+      content: 'Contact Page',
+    });
+    this.meta.addTag({
+      name: 'keywords',
+      content:
+        'about, page, description, Heider, Angular, DevsCO, Angular Pro, HeiderDev',
+    });
+  }
+}
